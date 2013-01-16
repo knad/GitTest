029ebf65286b1e9d73e04ab484ade6e86081fcfa
knad <knad@knad.zf11@gmail.com>
2013-01-16 17:22:47 +0900

    commit Test

--- GitUploadTest/src/com/example/gituploadtest/MainActivity.java
+++ GitUploadTest/src/com/example/gituploadtest/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.gituploadtest;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn=(Button)findViewById(R.id.button1);
+        btn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Git upload test", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
